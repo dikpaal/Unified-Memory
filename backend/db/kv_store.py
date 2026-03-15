b1a110dc9bfd171e824e7413106f9b4c6bea3e3a
dikpaal <dikpaal@icloud.com>
2026-03-14 18:10:05 -0700

    add KV store with timestamps

---
+++ backend/db/kv_store.py
@@ -0,0 +1,50 @@
+from datetime import datetime, timezone
+from typing import List
+from collections import defaultdict
+
+
+from models.models import Memory
+
+class KVStore:
+
+    def __init__(self):        
+        """
+        key: platform
+        value: (Memory, timestamp)
+        """
+        
+        self.kv_store = defaultdict(list)
+        
+    def add_memory(self, platform: str, memory: Memory):
+        """
+        Add memory for `platform` with current timestamp
+        """
+        
+        if platform not in {'chatgpt', 'claude'}:
+            raise ValueError("Invalid platform")
+        
+        self.kv_store[platform].append((memory, datetime.now(timezone.utc)))
+        
+    def get_memories(self, platform: str, timestamp: datetime) -> List[Memory]:
+        """
+        Returns the memories for a platform after `timestamp`
+        """
+            
+        if platform not in {'chatgpt', 'claude'}:
+            raise ValueError("Invalid platform")
+        
+        memories = self.kvstore[platform]
+        
+        l, r = 0, len(memories) - 1
+        
+        while l < r:
+            mid = (l + r) // 2
+            
+            if memories[mid][1] < timestamp:
+                # move right
+                l = mid + 1
+            else:
+                # move left
+                r = mid
+                
+        return memories[l:]
